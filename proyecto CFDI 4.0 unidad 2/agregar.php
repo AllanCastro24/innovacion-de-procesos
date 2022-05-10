bde392f5e9029d1ed6cd31febb9b43b816b54aad
AllanCastro24 <alan.castro.1226.ac@gmail.com>
2022-05-10 13:04:25 -0600

    Se agregó backend, falta terminar base de datos, programar index_session y actualizar perfil

---
+++ proyecto CFDI 4.0 unidad 2/agregar.php
@@ -0,0 +1,17 @@
+<?php 
+    include "conexion.php";
+    $nombreUsuario = $_POST['nombreUsuario'];
+    $correo = $_POST['correo'];
+    $contrasenya = $_POST['contrasenya'];
+
+    if($nombreUsuario!=null && $correo!=null && $contrasenya!=null){
+        $sql = 'insert into usuarios values (null, "'.$nombreUsuario.'", "'.$correo.'", "'.$nombreUsuario.'", "'.$contrasenya.
+            '", null);';
+        mysqli_query($obj_conexion, $sql);
+        if($nombre = 1){
+            header("location:login.html");
+        }
+    }
+
+
+?>
